b5d407a644f6ca9abcf1ecbff662eaf7881a5d2c
西　治彦 <hanishi@me.com>
2017-10-03 07:44:35 +0900

    first commit

---
+++ src/main/java/io/soysource/appengine/HelloworldController.java
@@ -0,0 +1,13 @@
+package io.soysource.appengine;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world - springboot-appengine-standard!";
+    }
+}
